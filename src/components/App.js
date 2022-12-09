cad318d0f1b0cf2cebc656bf94c752bde3c14c86
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-09 19:37:46 +0000

    set up of Authorisation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import '../styles/app.css';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import AuthContext from '../utils/AuthContext';
+import Home from './Home';
+import Account from './Account';
+import Login from './Login';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/account"
+              element={user ? <Account /> : <Navigate to="/login" replace />}
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
