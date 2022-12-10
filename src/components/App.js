60ca8ffd92f7396717b530d756b81487de7a94be
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-10 15:12:16 +0000

    Removed dashboard as the home route

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Dashboard from './Dashboard';
-import Login from "./Login";
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+//import Dashboard from './Dashboard';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Dashboard />} /> */}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
