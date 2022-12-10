5894b122b1c66ff43bc5a34e887ac754629289a1
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-10 13:04:29 +0000

    Deleted unused files

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,21 @@
-// import '../styles/app.css';
-// import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-// import { useState } from 'react';
-// import AuthContext from '../utils/AuthContext';
-// import Home from './Home';
-// import Account from './Account';
-// import Login from './Login';
-
-// function App() {
-//   const [user, setUser] = useState(null);
-//   return (
-//     <AuthContext.Provider value={{ user, setUser }}>
-//       <div className="app">
-//         <BrowserRouter>
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route
-//               path="/account"
-//               element={user ? <Account /> : <Navigate to="/login" replace />}
-//             />
-//             <Route path="/login" element={<Login />} />
-//           </Routes>
-//         </BrowserRouter>
-//       </div>
-//     </AuthContext.Provider>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-import ContainerLogin from "../containers/ContainerLogin";
-import Account from "../components/Account";
+import ContainerLogin from '../containers/ContainerLogin';
 import Dashboard from './Dashboard';
-import Header from "./Header";
-import Home from "./Home";
-import LoggedInPage from "./LoggedInPage"
-import Login from "./Login";
-import Preferences from "./Preferences";
-import { BrowserRouter, Route, Routes, Router } from 'react-router-dom';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/" element={''} />
-        <Route path="/" element={''} />
-        <Route path="/" element={''} />
-        <Route path="/" element={''} />
-      </Routes>
-    </Router>
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/ContainerLogin" element={<ContainerLogin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+  );
 }
 
 export default App;
